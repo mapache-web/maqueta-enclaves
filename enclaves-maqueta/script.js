48be8b5ba9b94695ad10a8ba688f88d17b90c6a1
Fedomus <federico.liousin@gmail.com>
2024-07-16 22:32:38 -0300

    Se le da funcionalidad a la navbar y faltan acomodar estilos

---
+++ enclaves-maqueta/script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    menuNavbar();
+
+})
+
+function menuNavbar() {
+
+    let iconoMenu = document.getElementById("icono-menu");
+    let navbar = document.getElementById("navbar");
+    let cerrarMenu = document.getElementById("cerrar-menu");
+    let wrapper = document.getElementById("wrapper");
+    let header = document.getElementById("header");
+
+    window.onresize = function () {
+
+        if (window.innerWidth > 768) {
+
+            if (wrapper.children.length === 4) {
+                wrapper.removeChild(wrapper.lastChild)
+            }
+
+            header.appendChild(navbar);
+            navbar.style.display = "flex";
+            navbar.className = "navbar-desktop";
+        }
+
+        if (window.innerWidth < 768) {
+
+            if (wrapper.children.length === 3) {
+                wrapper.appendChild(navbar)
+            }
+            navbar.style.display = "none";
+            navbar.className = "navbar-mobile"
+        }
+    }
+
+    iconoMenu.onclick = function () {
+
+        navbar.style.display = "block";
+
+        cerrarMenu.onclick = function () {
+
+            navbar.style.display = "none";
+
+        }
+
+    }
+
+}
