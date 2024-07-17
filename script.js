c94966fa66aa302db4a9686df7b2c78d40827aeb
Federico Limousin <federico.limousin@gmail.com>
2024-07-17 15:53:02 -0300

    arreglos sobre la navbar responsive

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
+"use strict";
+
 document.addEventListener("DOMContentLoaded", function () {
 
-    loadNavbar();
+    navbarResponsive();
 
 })
 
-function loadNavbar() {
+function navbarResponsive() {
 
     let iconoMenu = document.getElementById("icono-menu");
     let navbar = document.getElementById("navbar");
     let cerrarMenu = document.getElementById("cerrar-menu");
     let wrapper = document.getElementById("wrapper");
     let header = document.getElementById("header");
-
-    window.onresize = function () {
-
+    
+    function definirEstilos() {
         if (window.innerWidth > 768) {
 
             if (wrapper.children.length === 4) {
-                wrapper.removeChild(wrapper.lastChild)
+                wrapper.removeChild(wrapper.lastChild);
             }
-
+    
             header.appendChild(navbar);
             navbar.style.display = "flex";
             navbar.className = "navbar-desktop";
         }
-
+    
         if (window.innerWidth < 768) {
-
+    
             if (wrapper.children.length === 3) {
-                wrapper.appendChild(navbar)
+                wrapper.appendChild(navbar);
             }
             navbar.style.display = "none";
-            navbar.className = "navbar-mobile"
+            navbar.className = "navbar-mobile";
         }
     }
 
+    definirEstilos();
+
+    window.onresize = definirEstilos;
+
     iconoMenu.onclick = function () {
-
         navbar.style.display = "block";
-
     }
 
     cerrarMenu.onclick = function () {
-
         navbar.style.display = "none";
-
     }
-
 }
