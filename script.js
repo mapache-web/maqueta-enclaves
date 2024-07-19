a5e2c5f19359444744edc411f1fe33b811a633f1
Fedomus <federico.liousin@gmail.com>
2024-07-19 13:32:43 -0300

    ultimas correcciones a la navbar, falta arreglar el dropdown en version mobile

--- script.js
+++ script.js
@@ -13,45 +13,62 @@ function navbarResponsive() {
     let cerrarMenu = document.getElementById("cerrar-menu");
     let wrapper = document.getElementById("wrapper");
     let header = document.getElementById("header");
-    let itemsNavbar = document.getElementsByClassName("navLink")
+    let linksNavbar = document.getElementsByClassName("nav-link");
+    let linkFormacion = document.querySelector(".link-formacion");
+    let menuFormacion = document.querySelector('.menu-formacion');
+    let navbarImg = document.querySelector(".logo > img");
 
-    function cerrarNavbar(){
+    function cerrarNavbar() {
         navbar.style.display = "none";
     }
-    function abrirNavbar(){
+
+    function abrirNavbar() {
         navbar.style.display = "block";
     }
 
-    function eventosBotonesMobile(){
-        for (const item of itemsNavbar) {
+    function eventosBotonesMobile() {
+        for (const item of linksNavbar) {
             item.onclick = cerrarNavbar;
         }
+        linkFormacion.onclick = toggleMenuFormacion;
     }
 
-    function eventosBotonesDesktop(){
-        for (const item of itemsNavbar) {
-            item.onclick = (e) => {
-                e.preventDefault();
-            }
+    function eventosBotonesDesktop() {
+        for (const item of linksNavbar) {
+            item.onclick = "javascript:void(0)";
         }
+        linkFormacion.onclick = "javascript:void(0)";
     }
-    
-    function definirEstilos() {
+
+    function toggleMenuFormacion() {
+        menuFormacion.style.display = menuFormacion.style.display === 'block' ? 'none' : 'block';
+    }
+
+    function isScrolled(){
+        return window.scrollY > 5;
+    }
+
+    function onscrollStyles(){
+        header.className = isScrolled() ? "header header-on-scroll" : "header header-on-top";
+        navbarImg.style.maxHeight = isScrolled() ? "66px" : "170px";
+    }
+
+    function responsiveStyles() {
         if (window.innerWidth > 768) {
 
             if (wrapper.children.length === 4) {
                 wrapper.removeChild(wrapper.lastChild);
             }
-    
+
             header.appendChild(navbar);
             navbar.style.display = "flex";
             navbar.className = "navbar-desktop";
 
             eventosBotonesDesktop();
         }
-    
+
         if (window.innerWidth < 768) {
-    
+
             if (wrapper.children.length === 3) {
                 wrapper.appendChild(navbar);
             }
@@ -62,9 +79,11 @@ function navbarResponsive() {
         }
     }
 
-    definirEstilos();
+    responsiveStyles();
+    window.onresize = responsiveStyles;
 
-    window.onresize = definirEstilos;
+    onscrollStyles();
+    window.onscroll = onscrollStyles;
 
     iconoMenu.onclick = abrirNavbar;
 
